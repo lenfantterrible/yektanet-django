baacf34ff2ef90f419ee97e60ca05bbbee673781
lenfantterrible <mehdiat6@gmail.com>
2022-02-01 22:52:40 +0330

    Create Advertiser List View

--- Yektanet/advertiser_management/views.py
+++ Yektanet/advertiser_management/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.utils import timezone
+from django.views.generic.list import ListView
+
+from .models import Advertiser
+
+class AdvertiserListView(ListView):
+
+    model = Advertiser
+    context_object_name = 'advertisers'
+    template_name = 'ads.html'
+
