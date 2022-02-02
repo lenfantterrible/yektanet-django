e4512dbc17c741930f53933fd307591835b19d32
lenfantterrible <mehdiat6@gmail.com>
2022-02-02 14:44:42 +0330

    Add Admin Models

--- Yektanet/advertiser_management/admin.py
+++ Yektanet/advertiser_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Advertiser, Ad 
 
-# Register your models here.
+@admin.register(Advertiser) 
+class AdvertiserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'clicks', 'views')
+    fields = ('name', )
+
+@admin.register(Ad) 
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('name', 'advertiser', 'link', 'clicks', 'views')
+    fields = ('name', 'link', 'img', 'advertiser')
